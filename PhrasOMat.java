4fb81555fb34e41f8c1d60564674ac86ea3a9ea9
EldorWildCodeSchool <eldor-rabe_student2021@wilder.school>
2021-09-13 15:08:33 +0200

    Updated the java class in several aspects

--- PhrasOMat.java
+++ PhrasOMat.java
@@ -1,16 +1,16 @@
 public class PhrasOMat {
 
-	//PhrasOMat als Übung für Arrays und den ersten Einsatz von Math.random
-
     public static void main(String[] args){
+    //Quelle: "JAVA - Von Kopf bis Fuß - O'Reilly-Verlag"
+
     //Drei Wortgruppen erstellen, aus denen ausgewählt wird
         String[] wortListeEins = {"verlässliche", "erfolgsorientierte", "webbasierte", "allumfassende",
-        "clevere", "kundenorientierte", "pfadkritische", "dynamische", "konkurrenzfähige", "verbeulte",
+        "clevere", "kundenorientierte", "pfadkritische", "dynamische", "konkurrenzfähige", "verteilte",
         "zielgerichtete"};
 
         String [] wortListeZwei = {"gepowerte", "haftende", "Mehrwert-", "zentrierte", "geclusterte", "proaktive",
         "Out-of-the-Box", "positionierte", "vernetzte", "fokussierte", "kraftvolle", "geordnete", "geteilte",
-        "kooperative", "beschleunigte", "Multi-Tier", "Enterprise-", "B2B", "Frontend-", "vermerkelte"};
+        "kooperative", "beschleunigte", "Multi-Tier", "Enterprise-", "B2B", "Frontend-"};
 
         String[] wortListeDrei = {"Schicht", "Endstufe", "Lösung", "Architektur", "Kernkompetenz", "Strategie",
         "Kooperation", "Ausrichtung", "Räumlichkeit", "Vision", "Dimension", "Mission"};
@@ -27,8 +27,8 @@ public class PhrasOMat {
         int rand3 = (int) (Math.random() * dreiLänge);
 
     // Bisl Input bzgl. Math.Random als Gedankenstütze
-        System.out.println("Math.random() gibt ein pseudozufälliges Double größer oder gleich 0.0 und kleiner als 1.0 zurück. Daher multiplizieren wir es mit der Länge (Anzahl Einträge) des Arrays: " + einsLänge + "  -- Beispiel für Math.random: " + rawRand1);
-        System.out.println("Math.random() wird daher in diesem Anwendungsfall zu einem int gecastet (int) (Math.random() * einsLänge): " + rand1);
+        System.out.println("Math.random() gibt ein pseudozufälliges Double größer oder gleich 0.0 und kleiner als 1.0 zurück. Daher multiplizieren wir es mit der Länge (Anzahl Einträge) des Arrays, z.B. " + einsLänge + "  -- Beispiel für Math.random: " + rawRand1);
+        System.out.println("Math.random() wird, weil es ein Double ist, in diesem Anwendungsfall zu einem int gecastet (int) (Math.random() * einsLänge): " + rand1 + ". Denn nur ein int-Wert kann die Index-Posotion eines Array angeben!");
 
     // eine Phrase zusammenbauen
         String phrase = wortListeEins[rand1] + " " +
@@ -37,6 +37,6 @@ public class PhrasOMat {
     // eine Phrase ausgeben
 
         System.out.println("");
-        System.out.println("Was wir brauchenm ist eine " + phrase);
+        System.out.println("Was wir brauchen, ist eine " + phrase + "!");
     }
 }
